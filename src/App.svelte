29a63dd65f39238fcc1dcb640643b0cb772f923e
MisileLaboratory <misileminecord@gmail.com>
2022-10-18 02:22:44 +0900

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,85 @@
 <script>
 	export let name;
+	import { reveal } from 'svelte-reveal';
+	import { fly } from 'svelte/transition';
+	let visible = {
+		projects2022: false,
+		orgs2022: false
+	};
+	let flyx = -40;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
+<h1 use:reveal={{ transition: "slide", reset: true, duration: 500 }}>klosie, misile's cat!</h1>
+<projectstitle use:reveal={{ transition: "slide", reset: true, duration: 1700}}>projects</projectstitle>
+<details id="details" use:reveal={{ transition: "slide", reset: true, duration: 2500}} bind:open={visible["projects2022"]}>
+	<summary>2022</summary>
+	{#if visible["projects2022"]}
+		<a transition:fly="{{ x: flyx, duration: 1000 }}">pull request check</a>
+	{/if}
+</details>
+<donatestitle use:reveal={{ transition: "slide", reset: true, duration: 1700}}>Donate me</donatestitle>
+<div class="donate">
+	<a use:reveal={{ transition: "slide", reset: true, duration: 2500}} href="https://toss.me/klosie">Toss</a>
+</div>
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+
+	#gistgithub {
+		margin-top: 20px;
+		margin-left: 17.4%;
+		font-size: 1.5em;
+		color: #6F38C5;
+	}
+
+	#details {
+		margin-left: 17.4%;
+		font-size: 1.5em;
+		color: #6F38C5;
+	}
+
+	.profile {
+		margin-left: 17.4%;
+		font-size: 1.5em;
+	}
+
+	.donate {
+		margin-left: 17.4%;
+		font-size: 1.5em;
+	}
+
+	projectstitle {
+		display: block;
+		color: #87A2FB;
+		font-size: 1.7em;
+		margin: auto;
+		margin-top: 50px;
+		margin-left: 17.4%;
+	}
+
+	donatestitle {
+		display: block;
+		color: #87A2FB;
+		font-size: 1.7em;
+		margin: auto;
+		margin-top: 50px;
+		margin-left: 17.4%;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
+		color: #87A2FB;
+		font-size: 3em;
 		font-weight: 100;
+		text-align: center;
+		display: block;
+		margin: auto;
+		margin-top: 10px;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	a {
+		display: block;
+	}
+
+	:global(body) {
+		background-color: #ADDDD0;
+		font-weight: 100;
 	}
 </style>
